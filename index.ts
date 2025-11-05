29432a9c2fcb8b9c698e1bd1b479793063823678
r4b2 <github@r4b2.de>
2025-11-05 01:58:01 +0100

    fix(prettier): fmt

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
-});
+    port: 3000,
+    fetch(request) {
+        return new Response('Welcome to Bun!')
+    },
+})
 
-console.log(`Listening on ${server.url}`);
+console.log(`Listening on ${server.url}`)
